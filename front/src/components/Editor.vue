0fadf9c9df063bfb996a21c8aeb33e0e6fdca8e1
chenxd <chenxindong2020@163.com>
2021-11-11 23:49:41 +0800

    完成富文本编辑页面

---
+++ front/src/components/Editor.vue
@@ -0,0 +1,77 @@
+<template lang="html">
+  <div>
+    <el-row :gutter="20" class="center">
+      <div style="padding:20px">
+        <el-avatar :size="150" src="../../static/image/headLogo.jpg"></el-avatar>
+      </div>
+    </el-row>
+  
+    <div class="editor">
+      <div ref="toolbar" class="toolbar"></div>
+      <div ref="editor" class="text"></div>
+    </div>
+    <el-row :gutter="20" style="padding-top:20px" class="center">
+      <el-button type="primary" size="default" @click="submitContent">
+        保存
+      </el-button>
+  
+      <el-button type="success" size="default" @click="submitContent">
+        提交
+      </el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import E from "wangeditor";
+  export default {
+    data() {
+      return {
+        // uploadPath,
+        editor: null,
+        value: "东东爱小雅"
+      };
+    },
+  
+    mounted() {
+      this.seteditor();
+      this.editor.txt.html(this.value);
+    },
+    methods: {
+      click() {
+        alert(this.value);
+      },
+      seteditor() {
+        this.editor = new E(this.$refs.toolbar, this.$refs.editor);
+  
+        this.editor.config.onchange = html => {
+          this.value = html; // 绑定当前逐渐地值
+        };
+        // 创建富文本编辑器
+        this.editor.create();
+      }
+    }
+  };
+</script>
+
+<style lang="css">
+  .editor {
+    width: 60%;
+    margin: 0 auto;
+    position: relative;
+    z-index: 0;
+  }
+  
+  .toolbar {
+    border: 1px solid #ccc;
+  }
+  
+  .text {
+    border: 1px solid #ccc;
+    min-height: 300px;
+  }
+  
+  .center {
+    text-align: center;
+  }
+</style>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import MainPage from '@/components/MainPage'
+import Editor from '@/components/Editor'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'MainPage',
+      component: MainPage
+    },
+    {
+      path: '/editor',
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
